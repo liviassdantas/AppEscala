c1a47f95d202d46f6afa6969b960690faebda936
Livia Dantas <livia.ss.dantas@outlook.com>
2024-12-27 12:11:35 -0300

    Adicionando as pastas de serviço

---
+++ UserServiceTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UserServiceTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
