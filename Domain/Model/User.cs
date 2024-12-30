46f29d15c2a6e77d9f785b6f38df26bfa3a4892a
Livia Dantas <livia.ss.dantas@outlook.com>
2024-12-29 22:11:08 -0300

    Começando os serviços

---
+++ Domain/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BirthdayDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public Email Email { get; set; }
+
+    }
+}
--- UserServiceTest/UserServiceTest.cs
+++ UserServiceTest/UserServiceTest.cs
@@ -10,15 +10,16 @@ namespace UserServiceTest
         {
             User user =
                 new User("Fulano",
-                "04/03/1997)",
+                "04/03/1997",
                 "219999999",
                 new Email("fulano@gmail.com"),
                 Team_Music,
                 Function_Guitar_Player,
                 isLeader = false);
-            
-            
 
+            var userRepository = new UserRepository(IUnitOfWork _uow).SaveUser(user);
+            
+            Assert.IsTrue(userRepository.Exists(user));
         }
     }
 }
