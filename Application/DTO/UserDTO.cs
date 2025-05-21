4b70f3ab44d15599f7335c8c7d0339a0306ba92c
Livia Dantas <livia.ss.dantas@gmail.com>
2025-05-20 23:10:16 -0300

    integration tests are working

--- Application/DTO/UserDTO.cs
+++ Application/DTO/UserDTO.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Enums;
+using Core.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,24 @@ namespace Application.DTO
 {
     public class UserDTO
     {
+        public UserDTO() { }
+        public UserDTO(
+           string name,
+           Email email,
+           PhoneNumber phoneNumber,
+           string birthdayDate,
+           Team team,
+           Team_Function team_Function,
+           bool isLeader = false)
+        {
+            Name = name;
+            Email = email.EmailAddress;
+            PhoneNumber = phoneNumber.Number;
+            BirthdayDate = birthdayDate;
+            Team = team;
+            Team_Function = team_Function;
+            IsLeader = isLeader;
+        }
         public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
---
+++ UserServiceTest/IntegrationTest.cs
@@ -0,0 +1,86 @@
+﻿using Application.DTO;
+using Application.Interfaces.Service;
+using Core.Entities;
+using Core.Enums;
+using Core.Interfaces;
+using Core.ValueObjects;
+using HolyEscalasWebService.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserServiceTest
+{
+    [TestFixture]
+    public class IntegrationTest
+    {
+        private Mock<IUserService> _mockUserService;
+        private UserController _userController;
+        private UserDTO _mockUser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockUserService = new Mock<IUserService>();
+            _userController = new UserController(_mockUserService.Object);
+            _mockUser = new UserDTO(
+               "Fulano2",
+               new Email { EmailAddress = "fulanodasilva2@gmail.com" },
+               new PhoneNumber { Number = "+5524994210951" },
+               "04/03/1997",
+               Team.Music_Team,
+               Team_Function.Music_Team_Guitar,
+               false
+           );
+        }
+        [Test]
+        public async Task SaveUser_ShouldReturnCreated_WhenSuccess()
+        {
+            _mockUserService.Setup(s => s.SaveUser(It.IsAny<UserDTO>()))
+                            .ReturnsAsync(new ServiceResult { Success = true });
+
+            var result = await _userController.SaveUser(_mockUser);
+
+            var createdResult = result as CreatedAtActionResult;
+            Assert.NotNull(createdResult);
+            Assert.That(((UserDTO)createdResult.Value).Name, Is.EqualTo("Fulano2"));
+        }
+
+        [Test]
+        public async Task SaveUser_ShouldReturnBadRequest_WhenFailure()
+        {
+            _mockUserService.Setup(s => s.SaveUser(It.IsAny<UserDTO>()))
+                            .ReturnsAsync(new ServiceResult { Success = false });
+
+            var result = await _userController.SaveUser(_mockUser);
+
+            Assert.IsInstanceOf<BadRequestResult>(result);
+        }
+
+        [Test]
+        public async Task GetUserByEmailOrPhone_ShouldReturnUserDTO()
+        {
+            var expectedUser = _mockUser;
+
+            _mockUserService.Setup(s => s.GetUserByEmailOrPhone("fulanodasilva2@gmail.com", null))
+                            .ReturnsAsync(expectedUser);
+
+            var result = await _userController.GetUserByEmailOrPhone("fulanodasilva2@gmail.com", null);
+
+            Assert.AreEqual(expectedUser.Name, result.Name);
+        }
+        [Test]
+        public async Task GetUserByEmailOrPhone_ShouldReturnNull_WhenUserNotFound()
+        {
+            _mockUserService.Setup(s => s.GetUserByEmailOrPhone("fulanodasilva2@gmail.com", null))
+                            .ReturnsAsync((UserDTO)null);
+            var result = await _userController.GetUserByEmailOrPhone("fulanodasilva2@gmail.com", null);
+            Assert.IsNull(result);
+        }
+
+    }
+}
